865e752a70fc9150b6da081cf1c7e6ff01bb3aac
teatang <534427931@qq.com>
2020-12-25 23:12:30 +0800

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tin
+
+go 1.15
---
+++ middleware/logger.go
@@ -0,0 +1,12 @@
+package middleware
+
+// func Logger() gee.HandlerFunc {
+// 	return func(c *gee.Context) {
+// 		// Start timer
+// 		t := time.Now()
+// 		// Process request
+// 		c.Next()
+// 		// Calculate resolution time
+// 		log.Printf("[%d] %s in %v", c.StatusCode, c.Req.RequestURI, time.Since(t))
+// 	}
+// }
